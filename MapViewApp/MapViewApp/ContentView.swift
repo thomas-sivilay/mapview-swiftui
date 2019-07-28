1b739e27e3c86e35c3c069f98e7d8ce95cb46168
Thomas Sivilay <thomas.sivilay@gmail.com>
2019-07-28 14:11:57 +1000

    Added xcode project

---
+++ MapViewApp/MapViewApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MapViewApp
+//
+//  Created by Thomas Sivilay on 28/7/19.
+//  Copyright © 2019 Thomas Sivilay. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif

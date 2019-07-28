50a25f26696a979110dd3279c786d0ae7c665342
Thomas Sivilay <thomas.sivilay@gmail.com>
2019-07-28 14:45:26 +1000

    Added annotations

--- MapViewApp/MapViewApp/MapView.swift
+++ MapViewApp/MapViewApp/MapView.swift
@@ -9,6 +9,18 @@
 import SwiftUI
 import MapKit
 
+final class LandmarkAnnotation: NSObject, MKAnnotation {
+    let id: String
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+
+    init(landmark: Landmark) {
+        self.id = landmark.id
+        self.name = landmark.name
+        self.coordinate = landmark.location
+    }
+}
+
 struct MapView: UIViewRepresentable {
     @Binding var landmarks: [Landmark]
     
@@ -17,5 +29,12 @@ struct MapView: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
+        updateAnnotations(from: uiView)
+    }
+    
+    private func updateAnnotations(from mapView: MKMapView) {
+        mapView.removeAnnotations(mapView.annotations)
+        let newAnnotations = landmarks.map { LandmarkAnnotation(landmark: $0) }
+        mapView.addAnnotations(newAnnotations)
     }
 }
